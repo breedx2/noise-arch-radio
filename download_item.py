6c7fa568d226073f9ccadc100b5c3e0db5286772
jason <jason@noisybox.net>
2023-11-18 22:41:46 -0800

    add download item

---
+++ download_item.py
@@ -0,0 +1,39 @@
+import hashlib
+import json
+import os
+import urllib.request
+
+def download_item(id):
+  meta_file = f'data/meta/{identifier}.json'
+  file = open(meta_file, 'rt')
+  meta = json.load(file)
+  file.close()
+
+  originals = [x for x in meta['files'] if x['source'] == 'original']
+
+  if not os.path.exists('data/items'):
+    os.mkdir('data/items')
+  if not os.path.exists(f'data/items/{id}'):
+    os.mkdir(f'data/items/{id}')
+
+  originals = [x for x in originals if not x["name"].endswith('.xml')]
+
+  for file in originals:
+    url = f'https://archive.org/download/{id}/{file["name"]}'
+    outfile = f'data/items/{id}/{file["name"]}'
+    
+    if os.path.exists(outfile):
+      with open(outfile, 'rb', buffering=0) as f:
+          sha = hashlib.file_digest(f, 'sha1').hexdigest()
+
+      if file['sha1'] == sha:
+        print(f'{outfile} exists and sha matches, skipping download')
+        continue
+
+    print(f'fetching {id} :: {file["name"]}')
+    urllib.request.urlretrieve(url, outfile)
+
+
+if __name__ == '__main__':
+  identifier = 'noise-arch_varfa-unk'
+  download_item(identifier)
