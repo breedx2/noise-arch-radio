a67a0b9298f554f6ceafaeaaae1d30b6609a622d
jason <jason@noisybox.net>
2023-11-21 19:31:44 -0800

    fix runscript

--- icecast/run-ezstream.sh
+++ icecast/run-ezstream.sh
@@ -5,17 +5,16 @@ MYDIR=$(pwd)
 if [ -e /.dockerenv ]; then
 	# in container
 	source env.sh
-	sed -i -e 
 	cp /etc/ezstream.xml.template /etc/ezstream.xml
-	sed -i -e "s/_ICECAST_HOSTNAME_/${ICECAST_HOSTNAME}/" /etc/icecast/ezstream.xml
-	sed -i -e "s/_ICECAST_USER_/${ICECAST_USER}/" /etc/icecast/ezstream.xml
-	sed -i -e "s/_ICECAST_PASS_/${ICECAST_PASS}/" /etc/icecast/ezstream.xml
+	sed -i -e "s/_ICECAST_HOSTNAME_/${ICECAST_HOSTNAME}/" /etc/ezstream.xml
+	sed -i -e "s/_ICECAST_CLIENT_USER_/${ICECAST_CLIENT_USER}/" /etc/ezstream.xml
+	sed -i -e "s/_ICECAST_CLIENT_PASS_/${ICECAST_CLIENT_PASS}/" /etc/ezstream.xml
 	/usr/bin/ezstream -c /etc/ezstream.xml
 else
 	# on host
 	#docker run --detach \
 	docker run -it --rm \
-		-v ${MYDIR}/env.sh:/env.sh
+		-v ${MYDIR}/env.sh:/env.sh \
 		-v ${MYDIR}/ezstream.xml:/etc/ezstream.xml.template \
 		-v ${MYDIR}/data:/data \
 		--name ezstream \
