e1e9d19ee543d71e0448229e88a6406dc528e4fc
jason <jason@noisybox.net>
2023-11-27 23:02:13 -0800

    stupid shell shit

--- icecast/playlist-program.sh
+++ icecast/playlist-program.sh
@@ -1,7 +1,7 @@
 python3 playlist.py
 
 # Download future files ahead of time
-./download_ahead.sh 2>&1 & > /dev/null
+./download_ahead.sh 2>&1 > /dev/null &
 
 # Clean up items more than 8 hours old
 find data/items/ -type d -ctime .33 | xargs rm -rf 2>&1 > /dev/null
